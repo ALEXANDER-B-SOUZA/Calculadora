38fc71da1bc27cba31a877a5e6fb61b2a9da40cd
Alexander Batista de Souza <119431061+ALEXANDER-B-SOUZA@users.noreply.github.com>
2023-02-17 21:06:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@ function inserir(num)
     const numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
+
 function limpar()
 {
     document.getElementById('resultado').innerHTML = "";
 }
+
 function voltar()
 {
     const resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
+
 function calcular()
 {
     const resultado = document.getElementById('resultado').innerHTML;
